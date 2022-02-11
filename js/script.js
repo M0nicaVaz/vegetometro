f314b6225f047e92d813f15ab541c1ddef6e85db
Monica Vaz <contatomonicavaz@gmail.com>
2022-02-11 13:20:58 -0300

    desktop version final

--- js/script.js
+++ js/script.js
@@ -1,72 +1,112 @@
-let submitBtn = document.querySelector('#send');
-let backBtn = document.querySelector('#goback');
-let divResult = document.querySelector('#div-result');
-let mainDiv = document.querySelector('#mainDiv');
-let adults = document.querySelector('#adults');
-let children = document.querySelector('#children');
-let hours = document.querySelector('#hours');
-let alchoolY = document.querySelector('#alchoolY');
-let alchoolN = document.querySelector('#alchoolN');
-let resultList = document.querySelector('#resultList');
+let submitBtn = document.querySelector("#send");
+let backBtn = document.querySelector("#goback");
+let divResult = document.querySelector("#div-result");
+let mainDiv = document.querySelector("#mainDiv");
+let adults = document.querySelector("#adults");
+let children = document.querySelector("#children");
+let hours = document.querySelector("#hours");
+let resultList = document.querySelector("#resultList");
 
+function bbqCalc() {
+  let totalGuests = Number(adults.value) + Number(children.value * 0.5);
 
-submitBtn.addEventListener("click", (e) =>{
-    e.preventDefault();
-    divResult.classList.remove('display-none');
-    mainDiv.classList.add('display-none');  
+  if (hours.value < 6) {
+    var qtyGarlic = totalGuests * 2;
+    var qtyVeggies = totalGuests * 2;
+    var qtyShroom = totalGuests * 60;
+    var qtyCorn = totalGuests * 1;
+    var qtyCheese = totalGuests * 60;
+    var qtySoda = totalGuests * 1;
+    var qtyAlchool = adults.value * 1.2;
+  } else {
+    qtyGarlic = totalGuests * 3;
+    qtyVeggies = totalGuests * 3;
+    qtyShroom = totalGuests * 90;
+    qtyCorn = totalGuests * 2;
+    qtyCheese = totalGuests * 90;
+    qtySoda = totalGuests * 1.5;
+    qtyAlchool = adults.value * 2;
+  }
 
-    qtyCalc();
-});
+  let garlicLi = document.createElement("li");
+  resultList.appendChild(garlicLi);
+  let garlicLiLiTxt = document.createTextNode(
+    "🥖 " + qtyGarlic + " PÃES DE ALHO"
+  );
+  garlicLi.appendChild(garlicLiLiTxt);
 
-backBtn.addEventListener("click", () =>{
-    divResult.classList.add('display-none');
-    mainDiv.classList.remove('display-none');  
+  let veggiesLi = document.createElement("li");
+  resultList.appendChild(veggiesLi);
+  let veggiesLiTxt = document.createTextNode(
+    "🍅 " + qtyVeggies + " ESPETINHOS DE LEGUMES*"
+  );
+  veggiesLi.appendChild(veggiesLiTxt);
 
-    while (resultList.firstChild){
-        resultList.firstChild.remove();
-    }
-})
+  let shroomLi = document.createElement("li");
+  resultList.appendChild(shroomLi);
+  let shroomLiTxt = document.createTextNode("🍄 " + qtyShroom + "g DE SHITAKE");
+  shroomLi.appendChild(shroomLiTxt);
 
-function qtyCalc(){
-    let totalGuests = Number(adults.value) + Number(children.value * 0.5);
+  let cornLi = document.createElement("li");
+  resultList.appendChild(cornLi);
+  let cornLiTxt = document.createTextNode(
+    "🌽 " + qtyCorn + " ESPIGAS DE MILHO"
+  );
+  cornLi.appendChild(cornLiTxt);
 
-    let qtyVeggies= (totalGuests * 2);
-    let veggiesLi = document.createElement('li');
-    resultList.appendChild(veggiesLi);
-    let veggiesLiLiTxt = document.createTextNode('🍅 ' + qtyVeggies + ' espetinhos de legumes');
-    veggiesLi.appendChild(veggiesLiLiTxt);
+  let cheeseLi = document.createElement("li");
+  resultList.appendChild(cheeseLi);
+  let cheeseLiTxt = document.createTextNode(
+    "🧀 " + qtyCheese + "g DE QUEIJO COALHO"
+  );
+  cheeseLi.appendChild(cheeseLiTxt);
 
-    let qtyShroom= (totalGuests * 60);
-    let shroomLi = document.createElement('li');
-    resultList.appendChild(shroomLi);
-    let shroomLiTxt = document.createTextNode('🍄 ' + qtyShroom + ' gramas de shitake');
-    shroomLi.appendChild(shroomLiTxt);
+  let sodaLi = document.createElement("li");
+  resultList.appendChild(sodaLi);
+  let sodaLiTxt = document.createTextNode(
+    "🥤 " + qtySoda + "L DE REFRIGERANTE"
+  );
+  sodaLi.appendChild(sodaLiTxt);
 
-    let qtyCorn= (totalGuests * 1);
-    let cornLi = document.createElement('li');
-    resultList.appendChild(cornLi);
-    let cornLiTxt = document.createTextNode('🌽 ' + qtyCorn + ' espigas de milho');
-    cornLi.appendChild(cornLiTxt);
-
-    let qtyCheese= (totalGuests * 60);
-    let cheeseLi = document.createElement('li');
-    resultList.appendChild(cheeseLi);
-    let cheeseLiTxt = document.createTextNode('🧀 ' + qtyCheese + ' gramas de queijo coalho');
-    cheeseLi.appendChild(cheeseLiTxt);
-
-    let qtySoda = (totalGuests * 1.5).toFixed(1);
-    let sodaLi = document.createElement('li');
-    resultList.appendChild(sodaLi);
-    let sodaLiTxt = document.createTextNode('🥤 ' + qtySoda + ' litros de refrigerante');
-    sodaLi.appendChild(sodaLiTxt);
-
- if (alchoolY.checked){
-   let qtyAlchool = (adults.value * 1.2);
-   let alchoolLi = document.createElement('li');
-   resultList.appendChild(alchoolLi);
-   let alchoolLiTxt = document.createTextNode('🍺 ' + qtyAlchool + ' litros de cerveja');
-   alchoolLi.appendChild(alchoolLiTxt);
- }
+  let alchoolY = document.querySelector("#alchoolY");
+  if (alchoolY.checked) {
+    let alchoolLi = document.createElement("li");
+    resultList.appendChild(alchoolLi);
+    let alchoolLiTxt = document.createTextNode(
+      "🍺 " + qtyAlchool + "L DE CERVEJA"
+    );
+    alchoolLi.appendChild(alchoolLiTxt);
+  }
 }
 
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (adults.value && hours.value && children.value) {
+    divResult.classList.remove("display-none");
+    mainDiv.classList.add("display-none");
+    bbqCalc();
+  } else {
+    let mainTitle = document.querySelector("#mainTitle");
+    let warning = document.createElement("span");
+    let warningTxt = document.createTextNode(
+      "Por favor, preencha todos os campos!"
+    );
+    warning.appendChild(warningTxt);
+    warning.classList.add("warning");
+    mainTitle.insertAdjacentElement("beforeend", warning);
+    setTimeout(() => warning.remove(), 1500);
+  }
+});
 
+backBtn.addEventListener("click", () => {
+  divResult.classList.add("display-none");
+  mainDiv.classList.remove("display-none");
+
+  while (resultList.firstChild) {
+    resultList.firstChild.remove();
+  }
+});
+
+document.querySelector("#suggestions-span").addEventListener("click", () => {
+  document.querySelector(".suggestions").classList.toggle("display-none");
+});
