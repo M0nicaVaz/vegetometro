7f60c34077b0767ba897c243c605f961d3971d4d
Monica Vaz <contatomonicavaz@gmail.com>
2022-02-11 10:47:09 -0300

    function qtyCalc init

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ let submitBtn = document.querySelector('#send');
 let backBtn = document.querySelector('#goback');
 let divResult = document.querySelector('#div-result');
 let mainDiv = document.querySelector('#mainDiv');
+let adults = document.querySelector('#adults');
+let children = document.querySelector('#children');
+let hours = document.querySelector('#hours');
+let alchoolY = document.querySelector('#alchoolY');
+let alchoolN = document.querySelector('#alchoolN');
+let resultList = document.querySelector('#resultList');
+
 
 submitBtn.addEventListener("click", (e) =>{
     e.preventDefault();
@@ -14,8 +21,52 @@ submitBtn.addEventListener("click", (e) =>{
 backBtn.addEventListener("click", () =>{
     divResult.classList.add('display-none');
     mainDiv.classList.remove('display-none');  
+
+    while (resultList.firstChild){
+        resultList.firstChild.remove();
+    }
 })
 
 function qtyCalc(){
-    return;
+    let totalGuests = Number(adults.value) + Number(children.value * 0.5);
+
+    let qtyVeggies= (totalGuests * 2);
+    let veggiesLi = document.createElement('li');
+    resultList.appendChild(veggiesLi);
+    let veggiesLiLiTxt = document.createTextNode('🍅 ' + qtyVeggies + ' espetinhos de legumes');
+    veggiesLi.appendChild(veggiesLiLiTxt);
+
+    let qtyShroom= (totalGuests * 60);
+    let shroomLi = document.createElement('li');
+    resultList.appendChild(shroomLi);
+    let shroomLiTxt = document.createTextNode('🍄 ' + qtyShroom + ' gramas de shitake');
+    shroomLi.appendChild(shroomLiTxt);
+
+    let qtyCorn= (totalGuests * 1);
+    let cornLi = document.createElement('li');
+    resultList.appendChild(cornLi);
+    let cornLiTxt = document.createTextNode('🌽 ' + qtyCorn + ' espigas de milho');
+    cornLi.appendChild(cornLiTxt);
+
+    let qtyCheese= (totalGuests * 60);
+    let cheeseLi = document.createElement('li');
+    resultList.appendChild(cheeseLi);
+    let cheeseLiTxt = document.createTextNode('🧀 ' + qtyCheese + ' gramas de queijo coalho');
+    cheeseLi.appendChild(cheeseLiTxt);
+
+    let qtySoda = (totalGuests * 1.5).toFixed(1);
+    let sodaLi = document.createElement('li');
+    resultList.appendChild(sodaLi);
+    let sodaLiTxt = document.createTextNode('🥤 ' + qtySoda + ' litros de refrigerante');
+    sodaLi.appendChild(sodaLiTxt);
+
+ if (alchoolY.checked){
+   let qtyAlchool = (adults.value * 1.2);
+   let alchoolLi = document.createElement('li');
+   resultList.appendChild(alchoolLi);
+   let alchoolLiTxt = document.createTextNode('🍺 ' + qtyAlchool + ' litros de cerveja');
+   alchoolLi.appendChild(alchoolLiTxt);
+ }
 }
+
+
